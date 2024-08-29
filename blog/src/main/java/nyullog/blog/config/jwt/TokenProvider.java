f0cd12876a7e3b20a296458106f4ea151309351b
nyulnyul <nyul0223@naver.com>
2024-08-29 09:56:36 +0900

    유효성 jwt 구현

---
+++ blog/src/main/java/nyullog/blog/config/jwt/TokenProvider.java
@@ -0,0 +1,80 @@
+package nyullog.blog.config.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import nyullog.blog.config.JwtProperties;
+import nyullog.blog.domain.User;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Set;
+
+@RequiredArgsConstructor
+@Service
+public class TokenProvider {
+    private final JwtProperties jwtProperties;
+
+    public String generateToken(User user, Duration expiredAt){
+        Date now = new Date();
+        return makeToken(new Date(now.getTime() + expiredAt.toMillis()), user);
+    }
+
+    private String makeToken(Date expiry, User user){ //1. 토큰을 만드는 메소드
+        Date now = new Date();
+
+        return Jwts.builder().setHeaderParam(Header.TYPE, Header.JWT_TYPE) //헤더 type을 JWT로 설정
+                .setIssuer(jwtProperties.getIssuer()) //내용 issuer를 propertise 파일에서 설정한 값으로 설정
+                .setIssuedAt(now) // 내용 발행 시간을 현재 시간으로 설정
+                .setSubject(user.getEmail()) // 내용 주제를 user의 email로 설정
+                .setExpiration(expiry) // 내용 만료 시간을 파라미터로 받은 값으로 설정
+                .claim("user", user) // 클레임 id : 유저 id
+                .signWith(SignatureAlgorithm.HS256,jwtProperties.getSecretKey()) // HS256방식으로 secretKey와 함께 해시값을 서명
+                .compact();
+    }
+
+    //2, 토큰 유효성 검증 메소드로 jwtProperties에 선언한 비밀값과 함께 복호화 진행
+
+    public boolean validToken(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(jwtProperties.getSecretKey())
+                    .parseClaimsJws(token);
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+
+    //3. 토큰에서 인증 정보 객체를 가져오는 메소드로 복호화 후 클레임을 가져오는 getClaims를 호출해 클레임 정보를 받아 토큰 기반으로 인증 정보를 생성
+    public Authentication getAuthentication(String token) {
+        Claims claims = getClaims(token);
+        Set<SimpleGrantedAuthority> authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return new UsernamePasswordAuthenticationToken(new org.springframework.security.core.userdetails.User(claims.getSubject
+                (), "", authorities), token, authorities);
+    }
+
+    //4. 토큰에서 유저 id를 가져오는 메소드로 비밀값으로 토큰을 복호화 후 getClaims를 호출해 클레임 정보를 반환 받고 클레임에서 id키로 저장된 값을 가져와 반환
+    public Long getUserId(String token) {
+        Claims claims = getClaims(token);
+        return claims.get("id", Long.class);
+    }
+
+    //5. 토큰에서 클레임을 가져오는 메소드
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(jwtProperties.getSecretKey())
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
---
+++ blog/src/test/java/nyullog/blog/config/jwt/TokenProviderTest.java
@@ -0,0 +1,98 @@
+package nyullog.blog.config.jwt;
+
+import io.jsonwebtoken.Jwts;
+import nyullog.blog.config.JwtProperties;
+import nyullog.blog.domain.User;
+import nyullog.blog.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import java.time.Duration;
+import java.util.Date;
+import java.util.Map;
+
+@SpringBootTest
+public class TokenProviderTest {
+    @Autowired
+    private TokenProvider tokenProvider;
+    @Autowired
+    private JwtProperties jwtProperties;
+    @Autowired
+    private UserRepository userRepository;
+
+    @DisplayName("generateToken(): 유저 정보와 만료 기간을 전달해 토큰 만들 수 있다.")
+    @Test
+    void generateToken() {
+        //given
+        User testUser = userRepository.save(User.builder().email("user@gmail.com").password("test").build());
+        //when
+        String token = tokenProvider.generateToken(testUser, Duration.ofDays(14));
+        //then
+        Long userId = Jwts.parser()
+                .setSigningKey(jwtProperties.getSecretKey())
+                .parseClaimsJws(token)
+                .getBody()
+                .get("id", Long.class);
+
+        assertThat(userId).isEqualTo(testUser.getId());
+
+    }
+    @DisplayName("validToken(): 만료된 토큰일땐 유효성 검증 실패.")
+    @Test
+    void validToken_invaildToken(){
+        //given
+        String token = JwtFactory.builder()
+                .expiration(new Date(new Date().getTime() - Duration.ofDays(7).toMillis()))
+                .build()
+                .createToken(jwtProperties);
+        //when
+        boolean result = tokenProvider.validToken(token);
+        //then
+        assertThat(result).isFalse();
+    }
+    @DisplayName("validToken(): 유효한 토큰일땐 유효성 검증 성공.")
+    @Test
+    void validToken_validToken(){
+        //given
+        String token = JwtFactory.withDefaultValues().createToken(jwtProperties);
+        //when
+        boolean result = tokenProvider.validToken(token);
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("getAuthentication(): 토큰을 전달하면 인증 정보를 가져올 수 있다.")
+    @Test
+    void getAuthentication() {
+        //given
+        String userEmail = "user@gmail.com";
+        String token = JwtFactory.builder()
+                .subject(userEmail)
+                .build()
+                .createToken(jwtProperties);
+        //when
+        Authentication authentication = tokenProvider.getAuthentication(token);
+        //then
+        assertThat(((UserDetails) authentication.getPrincipal()).getUsername()).isEqualTo(userEmail);
+    }
+
+    @DisplayName("getUserId(): 토큰을 전달하면 유저 id를 가져올 수 있다.")
+    @Test
+    void getUserId() {
+        //given
+        Long userId = 1L;
+        String token = JwtFactory.builder()
+                .claims(Map.of("id", userId))
+                .build()
+                .createToken(jwtProperties);
+        //when
+        Long userIdByToken = tokenProvider.getUserId(token);
+        //then
+        assertThat(userIdByToken).isEqualTo(userId);
+    }
+}
